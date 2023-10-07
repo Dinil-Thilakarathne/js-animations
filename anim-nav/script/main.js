8f4147777114c29f6dd431d70029ffb55218cf73
Dinil Thilakarathne <sonacode44@gmail.com>
2023-10-08 00:43:38 +0530

    update some changes to anim-nav

--- anim-nav/script/main.js
+++ anim-nav/script/main.js
@@ -1,1 +1,54 @@
 import '../scss/style.scss'
+import {gsap} from 'gsap'
+
+const navIcon = document.querySelector('.nav-icon')
+const overlay = document.querySelector('.overlay');
+const hero  = document.querySelector('.hero')
+const heroContent = document.querySelector('.hero-content')
+const menuItems = gsap.utils.toArray('ul li a')
+
+let menuOpen = false;
+
+gsap.set(overlay, {
+    scaleY: 0,
+})
+gsap.set(menuItems, {
+    yPercent: 100
+})
+
+const tl = gsap.timeline({
+    defaults: {
+        ease: 'power4.inOut',
+        duration: 1
+    }
+});
+
+tl
+.to(hero, {
+    scale: 0.95,
+})
+.to(heroContent, {
+    pointerEvents: 'none'
+}, "<")
+.to(overlay, {
+    scaleY: 1
+}, "-=0.5")
+.to(navIcon, {
+    color: '#fff',
+}, "-=.9")
+.to(menuItems, {
+    yPercent: 0,
+    stagger: 0.1,
+    duration: 2
+}, "-=1.5")
+tl.pause();
+
+navIcon.addEventListener('click', () => {
+    if (!menuOpen) {
+        tl.play();
+        menuOpen = true;
+    } else {
+        tl.reverse();
+        menuOpen = false;
+    }
+})
