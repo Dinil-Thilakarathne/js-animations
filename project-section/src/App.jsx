04482618e8321d35571e47dd0ebe7eae248bf59e
Dinil Thilakarathne <sonacode44@gmail.com>
2023-10-11 18:45:24 +0530

    create new project project-section

---
+++ project-section/src/App.jsx
@@ -0,0 +1,26 @@
+const App = () => {
+
+    const projectContents = [
+        {
+            projectName : 'project 1',
+            projectTags : [
+                'web design'
+            ],
+            projectRating : '07',
+            projectColors : [
+                '#1659EA', '#FE7D68', '#5FB983'
+            ],
+            projectTypography : 'DM Sans',
+            projectTextWeights : [
+                'Regular', 'Medium', 'Bold'
+            ],
+            projectImg : ''
+        }
+    ]
+
+    return (
+        <></>
+    )
+}
+
+export default App
