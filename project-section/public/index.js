295f752b0114c50b47b41f1a2e49751083bfce83
Dinil Thilakarathne <sonacode44@gmail.com>
2023-10-12 07:26:46 +0530

    updates project-section project files

---
+++ project-section/public/index.js
@@ -0,0 +1,9 @@
+import project1Image from '/project-1.png'
+import project2Image from '/project-2.png'
+import project3Image from '/project-3.png'
+
+export {
+    project1Image,
+    project2Image,
+    project3Image
+}
--- project-section/src/App.jsx
+++ project-section/src/App.jsx
@@ -1,3 +1,6 @@
+import { project1Image, project2Image, project3Image } from "../public"
+import '../scss/style.scss'
+
 const App = () => {
 
     const projectContents = [
@@ -6,7 +9,7 @@ const App = () => {
             projectTags : [
                 'web design'
             ],
-            projectRating : '07',
+            projectRating : '08',
             projectColors : [
                 '#1659EA', '#FE7D68', '#5FB983'
             ],
@@ -14,12 +17,106 @@ const App = () => {
             projectTextWeights : [
                 'Regular', 'Medium', 'Bold'
             ],
-            projectImg : ''
+            projectImg : project1Image
+        },
+        {
+            projectName : 'project 2',
+            projectTags : [
+                'web design', 'brand design'
+            ],
+            projectRating : '09',
+            projectColors : [
+                '#D46374', '#F1C072', '#EDE2DA'
+            ],
+            projectTypography : 'DM Sans',
+            projectTextWeights : [
+                'Regular', 'Medium', 'Bold'
+            ],
+            projectImg : project2Image
+        },
+        {
+            projectName : 'project 3',
+            projectTags : [
+                'web design'
+            ],
+            projectRating : '08',
+            projectColors : [
+                '#ECEFF2', '#363851', '#2F2F2F'
+            ],
+            projectTypography : 'DM Sans',
+            projectTextWeights : [
+                'Regular', 'Medium', 'Bold'
+            ],
+            projectImg : project3Image
         }
     ]
 
     return (
-        <></>
+        <div className="project-section">
+            {projectContents.map(project => {
+                return (
+                    <div className={`project ${project.projectName}`}>
+                        <div className="project-top">
+                            <div className="project-top__line"></div>
+                            <div className="project-top__content">
+                                <p className="project-name">
+                                    {project.projectName}
+                                </p>
+                                <div className="project-tags">
+                                    {project.projectTags.map(tag => {
+                                        return (
+                                            <div className="tag">
+                                                {tag}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <p>
+                                    {project.projectRating}/10
+                                </p>
+                            </div>
+                        </div>
+                        <div className="project-bottom">
+                            <div className="project-bottom__content">
+                                <div className="colors-section">
+                                    <h3>Colors</h3>
+                                    <div className="color-boxes">
+                                        {project.projectColors.map(color => {
+                                            return (
+                                                <div className="color-box" style={{backgroundColor: `${color}`}}>
+                                                    <span>
+                                                        {color}
+                                                    </span>
+                                                </div>
+                                            )
+                                        })} 
+                                    </div>
+                                </div>
+                                <div className="text-section">
+                                    <div className="font-family">
+                                        <h3>Typography</h3>
+                                        {project.projectTypography}
+                                    </div>
+                                    <div className="font-weights">
+                                        <h3>Weights</h3>
+                                        {project.projectTextWeights.map((weight, index) => {
+                                            return (
+                                                <span>
+                                                    {weight}
+                                                </span>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project-bottom__img">
+                                <img src={project.projectImg} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
