423334fb6dd9811fc41400c2879cebe17c4f4539
Dinil Thilakarathne <sonacode44@gmail.com>
2024-01-22 14:23:53 +0530

    add new files

---
+++ scroll-reveal-css-only/main.js
@@ -0,0 +1,11 @@
+import './style.scss'
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
